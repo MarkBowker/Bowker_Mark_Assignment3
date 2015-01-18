813ad553ab412269f0a425a91d30c38db7ec681b
MarkBowker <MrBowker@Fullsail.edu>
2015-01-18 11:42:56 -0600

    created 3 functions for the amount of o2 left after killing certain Sharks.

--- Bowker_Mark_Assignment3.js
+++ Bowker_Mark_Assignment3.js
@@ -1,4 +1,54 @@
-alert("JavaScript works!");
-//commit test
-//branch
+//alert("JavaScript works!");
+
+var oxygenTime = 45;
+var spears = false;
+var oxygen = true;
+var greatWhite = 7.5;
+var tigerShark = 5;
+var blueShark = 2.5;
+var sharkNames =["Great White", "Tiger", "Blue Shark"]
+
+//Procedures
+//Function1 Kill a Great White
+
+var killGreatWhite = function (numberOfGreatWhites) {
+	if (oxygenTime > 0) {
+		oxygenTime = oxygenTime - (greatWhite * numberOfGreatWhites)
+		console.log("You have " + oxygenTime)
+			if (oxygenTime < 0) {
+			console.log("out");
+			return oxygenTime;
+			}
+	}
+};
+
+//Function 2 Kill a Tiger Shark
+
+var killTigerShark = function (numberOfTigerSharks) {
+	if (oxygenTime > 0) {
+		oxygenTime = oxygenTime - (tigerShark * numberOfTigerSharks)
+		console.log("You have " + oxygenTime)
+			if (oxygenTime < 0) {
+			console.log("out");
+			return oxygenTime;
+			}
+	}
+};
+
+//Function 3 Kill a Blue Shark
+
+var killBlueShark = function (numberOfBlueSharks) {
+	if (oxygenTime > 0) {
+		oxygenTime = oxygenTime - (blueShark * numberOfBlueSharks)
+		console.log("You have " + oxygenTime)
+			if (oxygenTime < 0) {
+			console.log("out");
+			return oxygenTime;
+			}
+	}
+};
+
+killBlueShark(10)
+
+
 
