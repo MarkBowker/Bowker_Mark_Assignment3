aef8dfcacf426b401bd8e80cb3168f32f0a3d6f7
MarkBowker <MrBowker@Fullsail.edu>
2015-01-19 16:42:14 -0600

    Made nested loop function

--- Bowker_Mark_Assignment3.js
+++ Bowker_Mark_Assignment3.js
@@ -1,38 +1,48 @@
 //alert("JavaScript works!");
 
 var oxygenTime = 45;
-var spears = false;
+var newTank = 45;
+var returnToSurface = false;
 var oxygen = true;
 var greatWhite = 7.5;
 var tigerShark = 5;
 var blueShark = 2.5;
 var sharkNames =["Great White", "Tiger", "Blue Shark"]
 
-//Procedures
+//Procedure Function: Return to the surface and get a new tank
+function getNewTank () {
+	var stillBreathing = newTank + oxygenTime
+		if (returnToSurface === true) {
+		console.log("You now have " + stillBreathing + " minutes to kill Sharks!" )
+			} else {
+			console.log("Then your oxygen left is " + oxygenTime + " minutes")
+			return stillBreathing;
+			}
+		};
+
 //Function1 Kill a Great White
 
 var killGreatWhite = function (numberOfGreatWhites) {
 	if (oxygenTime > 0) {
 		oxygenTime = oxygenTime - (greatWhite * numberOfGreatWhites)
-		console.log("You have " + oxygenTime)
-			if (oxygenTime < 0) {
+		console.log("You have " + oxygenTime + " to stay down and kill Sharks!")
+			} else if (oxygenTime < 0) {
 			console.log("out");
 			return oxygenTime;
 			}
-	}
+	
 };
-
 //Function 2 Kill a Tiger Shark
 
 var killTigerShark = function (numberOfTigerSharks) {
 	if (oxygenTime > 0) {
 		oxygenTime = oxygenTime - (tigerShark * numberOfTigerSharks)
-		console.log("You have " + oxygenTime)
-			if (oxygenTime < 0) {
+		console.log("You have " + oxygenTime + " to stay down and kill Sharks!")
+			} else if (oxygenTime < 0) {
 			console.log("out");
 			return oxygenTime;
 			}
-	}
+	
 };
 
 //Function 3 Kill a Blue Shark
@@ -40,15 +50,30 @@ var killTigerShark = function (numberOfTigerSharks) {
 var killBlueShark = function (numberOfBlueSharks) {
 	if (oxygenTime > 0) {
 		oxygenTime = oxygenTime - (blueShark * numberOfBlueSharks)
-		console.log("You have " + oxygenTime)
-			if (oxygenTime < 0) {
+		console.log("You have " + oxygenTime + " to stay down and kill Sharks!")
+			} else if (oxygenTime < 0) {
 			console.log("out");
 			return oxygenTime;
 			}
-	}
+	
 };
 
-killBlueShark(10)
+//Nested Loop for looking at stopwatch to check the time
+
+var checkStopwatch = function () {
+		oxygenTime = oxygenTime - 1
+		for (var min = oxygenTime; min === oxygenTime; min--){
+				if (oxygenTime = oxygenTime - 2){
+				break;} 
+				}
+			for (var seconds = 60; seconds >= 1; seconds--) {
+			console.log("You have approximately " + min + " minutes and " + seconds + " seconds before you run out of oxygen!")
+		
+}
+}
+
+checkStopwatch()
+
 
 
 
